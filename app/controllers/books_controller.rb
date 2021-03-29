1e985fbb43ed109933a8b4e9574849ef00f8b4c2
sisui-1124 <61412690+sisui-1124@users.noreply.github.com>
2021-03-29 23:59:02 +0900

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,20 @@
 class BooksController < ApplicationController
   def index
   end
+
   def new
     @book = Book.new
   end 
+
+  def create
+    book = Book.new(book_params)
+    book.save!
+    redirect_to books_url, notice: "ブック「＃｛book．name｝」を登録しました。"
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :description)
+  end
 end
