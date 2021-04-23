f25df248d2ec53fc2a2683e4fbe8d2107e140129
sisui-1124 <61412690+sisui-1124@users.noreply.github.com>
2021-04-23 23:51:26 +0900

    users model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  before_validation :set_nameless_name
   validates :name,  presence: true
   validate :validate_name_not_including_comma
 
@@ -6,5 +7,8 @@ class Book < ApplicationRecord
 
   def validate_name_not_including_comma
     errors.add(:name,'にカンマを含めることはできません') if name&.include?(',')
+  end
+  def set_nameless_name
+    self.name = '名前無し' if name.blank?
   end
 end
