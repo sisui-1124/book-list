e668e3c8682053033ad4b7baa35419a6ab1d05b8
sisui-1124 <61412690+sisui-1124@users.noreply.github.com>
2021-04-28 00:13:34 +0900

    Update users_controller.rb

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,22 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def edit
-  end
+  def create
+    @user = User.new(user_params)
 
-  def show
-  end
-
-  def index
+  if@user.save
+    redirect_to admin_user_url(@user), notice: "ユーザー「＃｛user.name｝」を登録しました"
+  else
+    render :new
   end
 end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+end
+end
+
