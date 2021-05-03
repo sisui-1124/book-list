c4f53a894f65c4671ac4d99fc11639feb13fb6f7
sisui-1124 <61412690+sisui-1124@users.noreply.github.com>
2021-05-03 23:54:53 +0900

    users2

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
-    @user = User.new
+    @user = User.find(params[:id])
   end
 
   def new
@@ -18,17 +18,18 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-  if@user.save
-    redirect_to admin_user_url(@user), notice: "ユーザー「＃｛user.name｝」を登録しました"
-  else
-    render :new
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{user.name}」を登録しました"
+    else
+      render :new
+    end
   end
 
   def update
     @user = User.find(params[:id])
 
-    if  @user.update(user_params)
-      redirect_to admin_user_url(@user), notice: "ユーザー「＃｛＠user．name｝」を更新しました。"
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました"
     else
       render :edit
     end
@@ -38,13 +39,13 @@ class Admin::UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_user_url, notice: "ユーザー「#{@user.name}」を削除しました。"
+    redirect_to admin_users_url, notice:"ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+    params.require(:user).permit( :name, :email, :admin, :password, :password_confirmation)
   end
 end
 
