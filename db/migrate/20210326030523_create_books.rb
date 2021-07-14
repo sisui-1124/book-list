b0c57ff82611a4dabc8835b5f940b1664c3d9646
sisui-1124 <61412690+sisui-1124@users.noreply.github.com>
2021-07-14 22:58:53 +0900

    Update 20210326030523_create_books.rb

--- db/migrate/20210326030523_create_books.rb
+++ db/migrate/20210326030523_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :name,  limit: 30, null: false
       t.text :description
+      t.text :image
 
       t.timestamps
       t.index :name, unique: true
